ebba2e48aa21e566d1b847b4948cb9c63246c34f
kai690 <k.green0519.ap@icloud.com>
2021-11-16 14:38:16 +0000

    [update]ログイン後のリダイレクト先を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_homes_path
+    when Customer
+      root_path
+    end
+  end
+  
 end
