6cfdc3aa6c85488a6685598ad09310c4d9452170
kai690 <k.green0519.ap@icloud.com>
2021-11-19 10:59:28 +0000

    [add]商品一覧画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,26 +1,37 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
+    @items = Item.all
   end
-  
+
   def show
   end
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path, notice:'商品が登録されました'
+    else
+      render :new
+    end
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
+  
+  def item_params
+    params.require(:item).permit(:genre, :name, :image, :introduction, :price, :is_active)
+  end
 end
