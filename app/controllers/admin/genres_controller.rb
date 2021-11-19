6596f9cdd606f4fd632865325481901506539ff6
kai690 <k.green0519.ap@icloud.com>
2021-11-19 06:05:23 +0000

    [Finish]ジャンル一覧・追加・編集・フラッシュメッセージ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,4 +1,5 @@
 class Admin::GenresController < ApplicationController
+
   def index
     @genres = Genre.all
     @genre = Genre.new
@@ -8,15 +9,30 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.new(genre_params)
 
     if @genre.save
-      redirect_to genres_path
+      redirect_to admin_genres_path, notice:'ジャンルが追加されました'
     else
       @genres = Genre.all
       render :index
     end
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+    redirect_to admin_genres_path, notice:'変更が保存されました'
+    else
+      render:edit
+    end
+  end
+
   private
+
   def genre_params
     params.require(:genre).permit(:name)
   end
+
 end
