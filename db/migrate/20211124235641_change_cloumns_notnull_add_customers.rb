0fe99c4b7686d97e0322326131b1760543ed94f1
kai690 <k.green0519.ap@icloud.com>
2021-11-27 09:25:43 +0000

    dbリセット

--- db/migrate/20211124235641_change_cloumns_notnull_add_customers.rb
+++ db/migrate/20211124235641_change_cloumns_notnull_add_customers.rb
@@ -1,11 +1,3 @@
 class ChangeCloumnsNotnullAddCustomers < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :customers, :first_name, false
-    change_column_null :customers, :last_name, false
-    change_column_null :customers, :last_name_kana, false
-    change_column_null :customers, :first_name_kana, false
-    change_column_null :customers, :postal_code, false
-    change_column_null :customers, :address, false
-    change_column_null :customers, :telephone_number, false
-  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_024207) do
+ActiveRecord::Schema.define(version: 2021_11_24_235641) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
