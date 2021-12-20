d0d445c1f59bee3c528b10912fc9871d7dc8aa32
kai690 <k.green0519.ap@icloud.com>
2021-12-20 13:04:26 +0000

    [add]注文情報入力画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,11 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
   end
+  def create
+    @order = Order.new(order_params)
+  end
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :address, :postal_code)
+  end
 end
