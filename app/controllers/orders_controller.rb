404ce5ded976f8352a3619c3b55a2f92646c2418
kai690 <k.green0519.ap@icloud.com>
2021-12-23 08:01:12 +0000

    [add]comfirm

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,29 +2,35 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def confirm
-    @select_address = params[:select_address]
+    @postage = 800
+    @total_price = 0
+    @billing_amount = @total_price + @postage
+    @cart_items = current_customer.cart_items
+    @select_address = params[:select_address].to_i
     @order = Order.new(order_params)
+
     if @select_address == 0
       @address = current_customer.address
     elsif @select_address == 1
       @address = Address.find(params[:order][:address_id])
     elsif @select_address == 2
-      @address = order_params[:address]
+      @address = order_params
     end
+
     @order.postal_code = @address.postal_code
     @order.address = @address.address
     @order.name = @address.first_name + @address.last_name
     
   end
-  
+
   def create
-    @order = Order.new
+
   end
-  
-  
-  
+
+
+
   private
   def order_params
     params.require(:order).permit(:payment_method, :address, :postal_code)
