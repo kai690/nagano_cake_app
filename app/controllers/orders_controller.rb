06f271ef44545943f51f7646fdd4f4787dd7d3bd
kai690 <k.green0519.ap@icloud.com>
2021-12-25 05:09:33 +0000

    [add]orders#create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,16 +27,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new
+    @order.save
     current_customer.cart_items.each do |cart_item|
       order_item = OrderItem.new
       order_item.item_id = cart_item.id
       order_item.order_id = @order.id
       order_item.amount = cart_item.amount
-      order_item.purchase_price = cart_item.price
+      order_item.purchase_price = cart_item.item.price
       order_item.save
     end
-    @order.save
-    redirect_to orders_tanks_path
+
+    redirect_to orders_thanks_path
   end
 
 
